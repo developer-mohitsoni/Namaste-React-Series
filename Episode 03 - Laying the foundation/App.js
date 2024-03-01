1a7fd92ec1e84d4b4c5eacb8a83443748334e975
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-01 18:55:06 +0530

    Intro to React Components and Deep Dive into Functional Component

--- Episode 03 - Laying the foundation/App.js
+++ Episode 03 - Laying the foundation/App.js
@@ -1,20 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// This JSX Code => Babel transpiles it to => React.createElement => ReactElement => Object => After rendering into DOM => HTML Element
-
-// Now, This JSX Code is transpiled into Babel to easily understand by JS Engine
-
-// If you JSX is in single line this is Perfectly valid JSX
-// If you write this JSX into multiple lines than you have to wrap it inside a parenthesis ()
-const jsxHeading = (
+//! React Element
+const heading = (
   <h1 className="head" tabIndex="1">
     Namaste React using JSX ✅
   </h1>
 );
 
-console.log(jsxHeading);
+console.log(heading); // Object
+
+//! React Components:-
+// Everything is a Component in a React.
+
+// What is Component:- A component is a reusable piece of code that is responsible for rendering a part of the UI.
+
+// 1. Class Based Component:- Old Way of writing code
+
+// 2. Function Component:- New Way of writing code. Just a normal JS Function which return some piece of JSX or something that can return React Element.
+
+  //? React Functional Component
+  const HeadingComponent = () => {
+    return <h1>Namaste React Functional Component</h1>;
+  };
+
+  //* Same:-
+
+  const HeadingComponent2 = () => <h1 className="heading">Namaste React Functional Component</h1>;
+
+  //* Same:-
+
+  const HeadingComponent3 = () => (
+    <h1 className="heading">Namaste React Functional Component</h1>
+  );
+
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(jsxHeading);
+root.render(heading);
