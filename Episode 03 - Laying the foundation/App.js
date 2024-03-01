b6cf27cb165ef8704d14e9887bcb98a0d73d2631
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-01 20:04:34 +0530

    Look how execution of any JS code inside JSX in Functional Component

--- Episode 03 - Laying the foundation/App.js
+++ Episode 03 - Laying the foundation/App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 //! React Element
 const heading = (
-  <h1 className="head" tabIndex="1">
-    Namaste React using JSX ✅
+  <h1 className="head1" tabIndex="1">
+    Namaste React By Akshay Saini ✅
   </h1>
 );
 
@@ -16,7 +16,7 @@ console.log(heading); // Object
 
 const Title = function () {
   return (
-    <h1 className="head" tabIndex="5">
+    <h1 className="head2" tabIndex="5">
       Namaste React using JSX 🚀
     </h1>
   );
@@ -24,8 +24,15 @@ const Title = function () {
 
 // How to render Title Component inside this HeadingComponent?
 //! Component Comosition
+
+//? We can write any JavaScript expression inside the {} in JSX
+const name = "Anurag Singh ProCodrr";
 const HeadingComponent = () => (
   <div id="container">
+    {/* {number} */}
+    <h2>Hello {name}</h2>
+    <h2>{500 + 600}</h2>
+    <h2>{console.log("Hello Mohit Soni")}</h2>
     <Title />
     <h1 className="heading">Namaste React Functional Component</h1>
   </div>
