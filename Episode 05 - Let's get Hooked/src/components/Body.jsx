ab4a06720977762985b5548f4ca01a71cfd81109
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-10 18:14:37 +0530

    Understand 'import' and 'export' with it's Types and make a Separate folder for Components  and Conventions to use Folders AND create a separate file for constant data,varibales and expression inside 'utils' folder

---
+++ Episode 05 - Let's get Hooked/src/components/Body.jsx
@@ -0,0 +1,24 @@
+import RestaurantCard from "./RestaurantCard";
+import restList from "../utils/mockData";
+import "../../index.css"
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div
+        className="rest-container"
+        style={{ display: "flex", flexWrap: "wrap", gap: "75px" }}
+      >
+        {/* Looping restList using map */}
+        {restList.map((restaurant) => (
+          //! You have to always mention unique key over here
+          <RestaurantCard key={restaurant.info.id} restData={restaurant} />
+
+          //* not using keys (not acceptable) <<<< index as key <<<<<<<<<<<<< unique id (best practice)
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
