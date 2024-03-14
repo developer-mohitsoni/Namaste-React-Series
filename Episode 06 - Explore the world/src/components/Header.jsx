17244c15e120cb2b430f00254bb9ced491e77502
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-14 15:28:05 +0530

    More Deep inside and Revise useState Hook

--- Episode 06 - Explore the world/src/components/Header.jsx
+++ Episode 06 - Explore the world/src/components/Header.jsx
@@ -1,7 +1,16 @@
 import Food from "../../assets/food.png";
+import { useState } from "react";
 import "../../index.css";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  //* But How this const value is updated when we click on button?
+  
+  //* when we re-render the component by click on the button, this btnNameReact will be a new variable and it will be re-rendered with new value
+
+  //! useState will re-render whole componenet when state changes
+  console.log("Header Render");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,7 +22,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
-          <button className="login">Login</button>
+          <button
+            className="login"
+            onClick={() => {
+              setBtnNameReact("Logout");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
