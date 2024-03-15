085eca1ecb0e6d7bbb8da1fd32a6cb9c00d06573
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-15 12:08:43 +0530

    If dependency array is empty then useEffect is called only initial render (just once)

--- Episode 07 - Finding the path/src/components/Header.jsx
+++ Episode 07 - Finding the path/src/components/Header.jsx
@@ -14,9 +14,12 @@ const Header = () => {
   //* useEffect() will be called every time my Header Component renders my useEffect is called.
 
   //* If no dependency array => useEffect is called on every render
+
+  //* If dependency arraay is empty = [] => useEffect is called on only initial render (just once)
+  
   useEffect(() => {
     console.log("useEffect called");
-  }); //* Here we put the dependency array over here. So, this dependency array changes the behaviour ofnit's render
+  }, []); //* Here we put the dependency array over here. So, this dependency array changes the behaviour ofnit's render
   return (
     <div className="header">
       <div className="logo-container">
