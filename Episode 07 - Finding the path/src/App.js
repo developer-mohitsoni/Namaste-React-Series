3f2ae0845695e07ef25dcb0fae730b996d7e788e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-15 12:51:29 +0530

    Implement Routing Basic Configuration in App.js file

--- Episode 07 - Finding the path/src/App.js
+++ Episode 07 - Finding the path/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import {createBrowserRouter} from "react-router-dom";
 
 //! React Components:-
 
@@ -16,6 +17,16 @@ const AppLayout = () => {
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />, // Load AppLayout Component when the route matches /
+  },
+  {
+    path: "/about",
+  }
+]);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<AppLayout />);
