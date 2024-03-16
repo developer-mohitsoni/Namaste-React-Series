a95d1c853df1065fead432fc7dcf91aec721940d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 11:51:55 +0530

    Functional Component

---
+++ Episode 08 - Let's get Classy/src/components/User.jsx
@@ -0,0 +1,10 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Mohit Soni</h2>
+      <h3>Location: UttarPradesh</h3>
+      <h4>Contact: developer_mohitsoni@outlook.com</h4>
+    </div>
+  );
+};
+export default User;
