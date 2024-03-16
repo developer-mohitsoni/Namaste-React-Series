24f99f76aa01daa254e58b23866a8a182232f78e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 14:51:23 +0530

    Destructure State varibale "count" in Class Based Component

--- Episode 08 - Let's get Classy/src/components/UserClass.jsx
+++ Episode 08 - Let's get Classy/src/components/UserClass.jsx
@@ -6,17 +6,18 @@ class UserClass extends React.Component {
 
     //* creating state variable of the class based component
     this.state = {
-      count: 0
+      count: 0,
     };
 
     console.log(props); //* Display props
   }
   render() {
     const { name, location, contact } = this.props;
+    const { count } = this.state;
 
     return (
       <div className="user-card">
-        <h1>Count: {this.state.count} </h1>
+        <h1>Count: {count} </h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
