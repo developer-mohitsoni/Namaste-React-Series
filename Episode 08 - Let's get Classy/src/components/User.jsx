abca295257ad7fc7f15cbf6d79764c382616f593
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 14:57:57 +0530

    Creating two state variables in Functional Based Component

--- Episode 08 - Let's get Classy/src/components/User.jsx
+++ Episode 08 - Let's get Classy/src/components/User.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 const User = ({ name }) => {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(1);
 
   return (
     <div className="user-card">
       <h1>Count = {count}</h1>
+      <h1>Count2 = {count2}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: UttarPradesh</h3>
       <h4>Contact: developer_mohitsoni@outlook.com</h4>
