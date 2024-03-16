5042e72e8ed59c93179a58c8f22d61a31b49e63e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 12:24:24 +0530

    Destructuring in Class Based Component as:-

--- Episode 08 - Let's get Classy/src/components/UserClass.jsx
+++ Episode 08 - Let's get Classy/src/components/UserClass.jsx
@@ -7,11 +7,13 @@ class UserClass extends React.Component {
     console.log(props);
   }
   render() {
+    const { name, location, contact } = this.props;
+
     return (
       <div className="user-card">
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: {this.props.location}</h3>
-        <h4>Contact: {this.props.contact}</h4>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h4>Contact: {contact}</h4>
       </div>
     );
   }
