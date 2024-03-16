3c30f991d511036c047b8495ed2307b7f863dfba
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 15:32:20 +0530

    Updating State varibale "count" in Functional Component

--- Episode 08 - Let's get Classy/src/components/User.jsx
+++ Episode 08 - Let's get Classy/src/components/User.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 
 const User = ({ name }) => {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(1);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  const handleClick2 = () => {
+    if (count > 0) setCount(count - 1);
+  };
 
   return (
     <div className="user-card">
       <h1>Count = {count}</h1>
-      <h1>Count2 = {count2}</h1>
+      <button onClick={handleClick}>Count Increase</button>&nbsp;&nbsp;
+      <button onClick={handleClick2}>Count Decrease</button>
       <h2>Name: {name}</h2>
       <h3>Location: UttarPradesh</h3>
       <h4>Contact: developer_mohitsoni@outlook.com</h4>
