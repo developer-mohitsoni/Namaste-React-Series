42dc01790127a706af08b8834a578f77650c63a6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 12:24:54 +0530

    Update formatter

--- Episode 08 - Let's get Classy/src/components/About.jsx
+++ Episode 08 - Let's get Classy/src/components/About.jsx
@@ -6,10 +6,14 @@ const About = () => {
     <div>
       <h1>About Us</h1>
       <h2>This is Namaste React WebSeries</h2>
-      <User name={"Mohit Soni (function)"} /> {/*Import Functional Based Component*/}
-
-
-      <UserClass name={"Mohit Soni (class)"} location= {"UttarPradesh"} contact = {"developer_mohitsoni@outlook.com"} /> {/*Import Class Based Component*/}
+      <User name={"Mohit Soni (function)"} />{" "}
+      {/*Import Functional Based Component*/}
+      <UserClass
+        name={"Mohit Soni (class)"}
+        location={"UttarPradesh"}
+        contact={"developer_mohitsoni@outlook.com"}
+      />{" "}
+      {/*Import Class Based Component*/}
     </div>
   );
 };
