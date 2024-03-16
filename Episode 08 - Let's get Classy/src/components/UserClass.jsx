55a4c653e630c35d29ee3922b4f363e7efcad400
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 14:45:43 +0530

    Creating State variable in Class Based Component

--- Episode 08 - Let's get Classy/src/components/UserClass.jsx
+++ Episode 08 - Let's get Classy/src/components/UserClass.jsx
@@ -4,13 +4,19 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log(props);
+    //* creating state variable of the class based component
+    this.state = {
+      count: 0
+    };
+
+    console.log(props); //* Display props
   }
   render() {
     const { name, location, contact } = this.props;
 
     return (
       <div className="user-card">
+        <h1>Count: {this.state.count} </h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
