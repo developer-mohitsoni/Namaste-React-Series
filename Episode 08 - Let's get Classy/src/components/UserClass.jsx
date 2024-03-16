88eb9f67ca3384790e0465a5127e84e4d7c59100
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 15:08:57 +0530

    Creating Two State Variables in Class Based Component

--- Episode 08 - Let's get Classy/src/components/UserClass.jsx
+++ Episode 08 - Let's get Classy/src/components/UserClass.jsx
@@ -7,17 +7,19 @@ class UserClass extends React.Component {
     //* creating state variable of the class based component
     this.state = {
       count: 0,
+      count2: 1,
     };
 
     console.log(props); //* Display props
   }
   render() {
     const { name, location, contact } = this.props;
-    const { count } = this.state;
+    const { count, count2 } = this.state;
 
     return (
       <div className="user-card">
         <h1>Count: {count} </h1>
+        <h1>Count2: {count2} </h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
