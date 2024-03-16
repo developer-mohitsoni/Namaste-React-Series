a711c95940eecace9eeae767cd79600680555f68
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-16 11:52:15 +0530

    Let's begin Class Based Component

--- Episode 08 - Let's get Classy/src/components/About.jsx
+++ Episode 08 - Let's get Classy/src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <h2>This is Namaste React WebSeries</h2>
+      <User /> {/*Import Functional Based Component*/}
+      <UserClass /> {/*Import Class Based Component*/}
     </div>
   );
 };
---
+++ Episode 08 - Let's get Classy/src/components/UserClass.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Mohit Soni</h2>
+        <h3>Location: UttarPradesh</h3>
+        <h4>Contact: developer_mohitsoni@outlook.com</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
