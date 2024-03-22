95b8db54e93df2283e21c8730b6e6e84f12be33e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-22 21:46:20 +0530

    Updating CartSlice

--- Episode 12 - Let's build our Store/src/utils/CartSlice.js
+++ Episode 12 - Let's build our Store/src/utils/CartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const CartSlice = createSlice({
   name: "cart",
@@ -7,14 +7,29 @@ const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      //* Mutating the state over here
+      //* Vanilla(older) Redux => DON'T MUTATE THE STATE
+      //* We uses like this in Older Redux:-
+
+      //* const newState = [...state];
+      //* newState.items.push(action.payload);
+      //* return newState;
+
+      //* Redux Toolkit using Immer behind the scene
+      //* We have to Mutating the state over here
       state.items.push(action.payload);
     },
     removeItem: (state) => {
       state.items.pop();
     },
+    
+    // originalState = {items: ["pizza", "burger", "coke"]}
     clearCart: (state) => {
-      state.items.length = 0; // []
+      console.log(current(state));
+
+      //? RTK - either Mutate the state or return a new state;
+      //* state.items.length = 0; // state = []
+
+      return { items: [] }; //* new original state will be empty as:-{items: []}
     },
   },
 });
