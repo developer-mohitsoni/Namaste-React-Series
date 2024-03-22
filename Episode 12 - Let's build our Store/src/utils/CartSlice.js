d7460d680999ff1b154ce40629d7736d7113b1c9
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-22 12:21:42 +0530

    Revise the Concept of CartSlice once

--- Episode 12 - Let's build our Store/src/utils/CartSlice.js
+++ Episode 12 - Let's build our Store/src/utils/CartSlice.js
@@ -1,21 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice = createSlice({
+const createSlice = createSlice({
   name: "cart",
-
-  //* what state initially of the cartState
   initialState: {
-    items: [], //*Initial State of our Slice
+    items: [],
   },
-
-  //* we will create action and reducer here. here we write reducer function corresponding to each action. Action is like add an item, remove the items and clear the items.
-
-  //* It's a kind of API that communicate with redux store.
-  //* This reducer is basically modifies the cart store because here we contains reducer function which as an two parameters inside it which able to modifiy the data.
   reducer: {
-    //* These is our reducer function
-
-    //* The two parameters "state" is a initialState and "action" that we are need to perform
     addItem: (state, action) => {
       state.items.push(action.payload);
     },
@@ -23,21 +13,10 @@ const cartSlice = createSlice({
       state.items.pop();
     },
     clearCart: (state) => {
-      state.items.length = 0;
+      state.items.length = 0; // []
     },
   },
 });
 
-/*
-{
-  actions: {
-    addItem;
-  },
-  reducer
-}
-*/
-//* We need to export two things here first all the actions that are done inside reducer function
 export const { addItem, removeItem, clearCart } = cartSlice.actions;
-
-//* Second export the reducer function
 export default cartSlice.reducer;
