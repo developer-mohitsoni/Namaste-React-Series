b20823533a5425a4ecc9405501f386af5f4136b2
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-23 13:32:54 +0530

    Designing the Contact Us Page

--- Episode 13 - Time for the Test/src/components/Contact.jsx
+++ Episode 13 - Time for the Test/src/components/Contact.jsx
@@ -1,8 +1,24 @@
-const Contact = ()=>{
-    return (
-        <div>
-            <h1>Contact Us</h1>
-        </div>
-    )
-}
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us</h1>
+      <form>
+        <input
+          type="text"
+          className="border-[1px] border-black p-2 m-2 shadow-md"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border-[1px] border-black p-2 m-2 shadow-md"
+          placeholder="message"
+        />
+
+        <button className="border-[1px] border-black p-2 m-2 bg-gray-200 rounded-lg shadow-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 export default Contact;
