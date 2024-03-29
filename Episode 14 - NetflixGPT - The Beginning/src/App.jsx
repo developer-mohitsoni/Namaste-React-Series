0299ca5919b4672fab2ea52d09ea8f0d3968643f
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-29 20:16:02 +0530

    Setup all Components

---
+++ Episode 14 - NetflixGPT - The Beginning/src/App.jsx
@@ -0,0 +1,6 @@
+import Body from "./components/Body";
+
+const App = () => {
+  return <Body />;
+};
+export default App;
---
+++ Episode 14 - NetflixGPT - The Beginning/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Browse from "./Browse";
+import Login from "./Login";
+
+const Body = () => {
+  return (
+    <div>
+      <Login />
+      <Browse />
+    </div>
+  );
+};
+
+export default Body;
