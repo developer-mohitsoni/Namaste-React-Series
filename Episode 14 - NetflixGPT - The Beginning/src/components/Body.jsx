888a59914647dd0f20e8289f2f8ab1dd576f839c
Mohit Soni <developer_mohitsoni@outlook.com>
2024-03-29 20:27:57 +0530

    Initialize "Browse Router" in Body Component

--- Episode 14 - NetflixGPT - The Beginning/src/components/Body.jsx
+++ Episode 14 - NetflixGPT - The Beginning/src/components/Body.jsx
@@ -1,11 +1,24 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
+import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter}>
+        <Outlet/>
+      </RouterProvider>
     </div>
   );
 };
