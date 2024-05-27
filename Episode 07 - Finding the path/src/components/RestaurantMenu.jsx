d07540ef9b2515d6f5da225231fd5afa06b4bc9d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-05-27 13:25:37 +0530

    Update Restaurant Menu API

--- Episode 07 - Finding the path/src/components/RestaurantMenu.jsx
+++ Episode 07 - Finding the path/src/components/RestaurantMenu.jsx
@@ -30,10 +30,11 @@ const RestaurantMenu = () => {
 
   // Yahan par mujhe dikkat aayi thi tabhi ye OR operator use kiya hai
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card;
 
   console.log(itemCards);
 
